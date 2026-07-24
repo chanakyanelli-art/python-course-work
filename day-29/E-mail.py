aa2db2d6e0ec5dee6874c2e481c4c08095136b10
Chanakya Nelli <chanakyanelli@gmail.com>
2026-07-24 16:48:26 +0530

    Add files via upload

---
+++ day-29/E-mail.py
@@ -0,0 +1,16 @@
+import smtplib
+import os
+import csv
+from email.mime.multipart imoprt MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PRT = 587
+SENDER_EMAIL = "chanakyanelli@gmail.com"
+SENDER_PASSWORD = "qmra dsfk maga zolp"
+
+
+
+
